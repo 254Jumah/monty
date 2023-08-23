de32214ddbdf8c50ddb9003b3fb387cf9d11e859
254Jumah <Jumahtitus@gmail.com>
2023-08-23 07:46:45 +0300

    learning by making mistakes

---
+++ main.c
@@ -0,0 +1,57 @@
+#include "monty.h"
+#include <ctype.h>
+#include <string.h>
+#define MAX_LENGTH 100
+/**
+ * main - ...
+ */
+int main(int argc, char **argv)
+{
+	FILE* file;
+	char opcode[5];
+	int l = 0, i = 0;
+	char val[MAX_LENGTH];
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while(fscanf(file, "%s", opcode) == 1)
+	{
+		l++;
+		trimSpaces(opcode);
+		if(strcmp(opcode, "push") == 0)
+		{
+			fscanf(file, "%s", val);
+			trimSpaces(val);
+			for (i = 0; val[i] != '$' && val[i] != '\0'; i++)
+			{
+				if (!isdigit(val[i]))
+				{
+					fprintf(stderr, "L%d: usage: push integer\n", l);
+					exit(EXIT_FAILURE);
+				}
+			}
+			push(atoi(val));
+		}
+		else if(strcmp(opcode, "pop") == 0)
+			pop();
+		else if(strcmp(opcode, "pall") == 0)
+			display();
+		else
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", l, opcode);
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (0);
+}
+
